e93883e6788001be7983ee1a2ef2c801fc0fa49c
bigzhu <vermiliondun@gmail.com>
2015-04-14 16:50:19 +0800

    加入括号补全

--- .vim/bundle/clone_git.sh
+++ .vim/bundle/clone_git.sh
@@ -6,3 +6,5 @@ git clone https://github.com/jistr/vim-nerdtree-tabs.git
 git clone https://github.com/groenewege/vim-less.git
 git clone https://github.com/kchmck/vim-coffee-script.git
 git clone https://github.com/evanmiller/nginx-vim-syntax
+git clone https://github.com/Raimondi/delimitMate # 括号补全
+
