c3c0ad1ebba3697c83928e7cb03d23990a6203d8
bigzhu <vermiliondun@gmail.com>
2016-02-20 11:48:32 +0800

    解决格式化的问题

--- .vim/bundle/clone_git.sh
+++ .vim/bundle/clone_git.sh
@@ -11,3 +11,4 @@ git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javas
 #git clone https://github.com/darthmall/vim-vue.git #vim vue
 git clone https://github.com/posva/vim-vue #vue 的高亮
 git clone https://github.com/jiangmiao/simple-javascript-indenter # 解决没有分号时候的自动格式化缩进
+git clone https://github.com/othree/html5.vim #完美格式化html以及里面的js css
